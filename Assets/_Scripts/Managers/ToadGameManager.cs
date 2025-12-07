5d32634c2f3a7f56633bc75b7d6c33b6c071fc22
Dbgraff <153063204+Dbgraff@users.noreply.github.com>
2025-12-07 23:16:03 +0300

    fix: buttons availible
    The buttons are available immediately after evolution

--- Assets/_Scripts/Managers/ToadGameManager.cs
+++ Assets/_Scripts/Managers/ToadGameManager.cs
@@ -229,10 +229,9 @@ public class ToadGameManager : MonoBehaviour
         }
 
         toadLevel++;
-        UpdateUI();
         toadObject.transform.localScale = originalScale * (1f + toadLevel * 0.1f);
         SetToadState(ToadState.Happy);
-
+        UpdateUI();
     }
 
     private IEnumerator ClickAnimation()
